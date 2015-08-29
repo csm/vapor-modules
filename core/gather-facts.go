2e723cb9211482f38d7da9c44ae5e1dc594d1e0b
Casey Marshall <cmarshall@lyveminds.com>
2015-08-29 11:18:37 -0700

    Initial commit.

---
+++ core/gather-facts.go
@@ -0,0 +1,72 @@
+package main
+import (
+	"github.com/csm/go-edn/types"
+	"vapor"
+	"runtime"
+	"net"
+	"container/list"
+)
+
+type GatherFacts struct{}
+
+func (self GatherFacts) TakesInput() bool {
+	return false
+}
+
+func (self GatherFacts) Exec(_ types.Value) (types.Value, error) {
+	var result types.Map = make(types.Map)
+	result[types.Keyword("os")] = types.String(runtime.GOOS)
+	result[types.Keyword("arch")] = types.String(runtime.GOARCH)
+	var interfaces, err = net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	var netifs = make(types.Vector, len(interfaces))
+	for i, iface := range interfaces {
+		var facemap = make(types.Map)
+		facemap[types.Keyword("name")] = types.String(iface.Name)
+		facemap[types.Keyword("mac-address")] = types.String(iface.HardwareAddr.String())
+		facemap[types.Keyword("loopback?")] = types.Bool(iface.Flags & net.FlagLoopback != 0)
+		var addrs, err = iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		var v4list = (*types.List)(list.New())
+		var v6list = (*types.List)(list.New())
+		for _, addr := range addrs {
+			var addrmap = make(types.Map)
+			switch ip := addr.(type) {
+			case *net.IPAddr:
+				var v4 = ip.IP.To4()
+				addrmap[types.Keyword("address")] = types.String(ip.IP.String())
+				if v4 != nil {
+					v4list.Insert(addrmap)
+				} else {
+					v6list.Insert(addrmap)
+				}
+				break
+
+			case *net.IPNet:
+				var v4 = ip.IP.To4()
+				addrmap[types.Keyword("address")] = types.String(ip.IP.String())
+				addrmap[types.Keyword("prefix-length")] = types.Int(ip.Mask.Size())
+				if v4 != nil {
+					v4list.Insert(addrmap)
+				} else {
+					v6list.Insert(addrmap)
+				}
+				break
+
+			}
+		}
+		facemap[types.Keyword("ipv4-addresses")] = v4list
+		facemap[types.Keyword("ipv6-addresses")] = v6list
+		netifs[i] = facemap
+	}
+	result[types.Keyword("network-interfaces")] = netifs
+	return result, nil
+}
+
+func main() {
+	vapor.RunModule(GatherFacts{})
+}
---
+++ module.go
@@ -0,0 +1,51 @@
+package vapor
+
+import (
+	"github.com/csm/go-edn"
+	"github.com/csm/go-edn/types"
+	"os"
+	"bufio"
+	"fmt"
+)
+
+type Module interface {
+	TakesInput() bool
+	Exec(types.Value) (types.Value, error)
+}
+
+func RunModule(module Module) {
+	var input types.Value = nil
+	var err error = nil
+	if module.TakesInput() {
+		input, err = edn.ParseReader(bufio.NewReader(os.Stdin))
+	}
+	if err != nil {
+		os.Exit(1)
+	}
+	var output types.Value = nil
+	output, err = module.Exec(input)
+	if err != nil {
+		if output != nil {
+			fmt.Print(edn.DumpString(output))
+		}
+		os.Exit(1)
+	}
+	if output != nil {
+		fmt.Println(edn.DumpString(output))
+	}
+	os.Exit(0)
+}
+
+type BoolType bool
+
+func (self BoolType) Equals(other types.Value) {
+	return self == other
+}
+
+func (self BoolType) String() string {
+	if self {
+		return "true"
+	} else {
+		return "false"
+	}
+}
